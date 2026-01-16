b85987301ee945d1a3502b935972a2d2014f8118
JasonZachariah <jasontzachariah@gmail.com>
2026-01-16 15:08:42 -0500

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,19 @@
+// Collapsible Code
+
+//Get name of the collapsible button
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    
+    var content = this.nextElementSibling;
+    console.log(content);
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
