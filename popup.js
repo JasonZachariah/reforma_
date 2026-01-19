2c71ef9b01ff28188eca1d1d3266590724e82ab5
Jason Zachariah <jasontzachariah@gmail.com>
2026-01-18 22:37:03 -0500

    Add button to pus orange color

--- popup.js
+++ popup.js
@@ -1,19 +1,25 @@
-// Collapsible Code
+const clickedButton = document.getElementById('clickedButton');
+const textbox = document.getElementById('textbox_id').value;
 
-//Get name of the collapsible button
-var coll = document.getElementsByClassName("collapsible");
-var i;
+console.log(textbox);
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    
-    var content = this.nextElementSibling;
-    console.log(content);
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+function injectedFunction() {
+  const divs  = document.querySelectorAll('div');
+
+divs.forEach(div => {
+  div.style.backgroundColor = "orange";
+});
 }
+
+clickedButton.addEventListener('click', async () => {
+
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+      target : {tabId : tab.id},
+      func : injectedFunction,
+    });
+  
+
+
+});
